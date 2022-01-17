f465583d7e71a9a5df73783b0e7189ddb6af2bf6
Roch288 <c0837288@mylambton.ca>
2022-01-17 17:49:20 -0500

    Initial Commit

---
+++ Lab1_RochBajracharya_C0837288_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab1_RochBajracharya_C0837288_iOS
+//
+//  Created by Roch on 17/01/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
