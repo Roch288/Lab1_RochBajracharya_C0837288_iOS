6987a8ca731e464c9fc1cd767cdb1116d2e5f6ea
Roch288 <c0837288@mylambton.ca>
2022-01-18 18:19:12 -0500

    Logic to check the winner and show message

--- Lab1_RochBajracharya_C0837288_iOS/ViewController.swift
+++ Lab1_RochBajracharya_C0837288_iOS/ViewController.swift
@@ -12,10 +12,18 @@ class ViewController: UIViewController {
     @IBOutlet weak var crossScoreLabel: UILabel!
     @IBOutlet weak var noughtScoreLabel: UILabel!
     @IBOutlet weak var turnLabel: UILabel!
+    @IBOutlet weak var message: UILabel!
     
     var turn = "X";
     var crossScore = 0
     var noughtScore = 0
+    
+    var noughtPosition = [Int]();
+    var crossPosition = [Int]();
+    
+    var winningPositions = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]];
+    
+    var hasWinner = false
     
     
     override func viewDidLoad() {
@@ -27,18 +35,53 @@ class ViewController: UIViewController {
     }
     
     @IBAction func tileClickHandler(_ sender: UIButton) {
-        // set image to cross or nought
-        sender.setImage(UIImage(named: self.turn == "X" ? "cross" : "nought"), for: .normal);
-        
-        // For fade in Animation
-        sender.imageView?.alpha = 0
-        
-        UIView.animate(withDuration: 1){
-            sender.imageView?.alpha = 1
+        if sender.image(for: .normal) == nil && message.text == "" {
+            // set image to cross or nought
+            sender.setImage(UIImage(named: self.turn == "X" ? "cross" : "nought"), for: .normal);
+            
+            // For fade in Animation
+            sender.imageView?.alpha = 0
+            
+            UIView.animate(withDuration: 1){
+                sender.imageView?.alpha = 1
+            }
+            
+            // store position of player
+            turn == "X" ? crossPosition.append(sender.tag) : noughtPosition.append(sender.tag)
+            
+            // check wheather any player has won
+            for positions in winningPositions {
+                if turn == "X" {
+                    let winningSet = Set(positions);
+                    let crossSet = Set(crossPosition);
+                    let isWinner = winningSet.isSubset(of: crossSet);
+                    if isWinner {
+                        message.text = "Congratulations! Cross is Winner"
+                        hasWinner = true
+                        crossScore += 1
+                        crossScoreLabel.text = String(crossScore)
+                    }
+                } else {
+                    let winningSet = Set(positions);
+                    let noughtSet = Set(noughtPosition);
+                    let isWinner = winningSet.isSubset(of: noughtSet);
+                    if isWinner {
+                        message.text = "Congratulations! Nought is Winner"
+                        hasWinner = true
+                        noughtScore += 1
+                        noughtScoreLabel.text = String(noughtScore)
+                    }
+                }
+                
+                if !hasWinner && (crossPosition.count + noughtPosition.count) == 9 {
+                    message.text = "It's a Draw"
+                }
+                
+            }
+            
+            // toggle player turns
+            turn = turn == "X" ? "O" : "X"
         }
-        
-        // toggle player turns
-        turn = turn == "X" ? "O" : "X"
     }
     
     
